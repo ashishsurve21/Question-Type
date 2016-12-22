25ddfd50f57a8b852921ac9f04550b37f5a05d0d
ashish surve <surve.ashish21@gmail.com>
2016-12-22 15:53:23 +0530

    Add files via upload

---
+++ QType.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 14:42:30 2016
+
+@author: Ashish
+"""
+import os
+import nltk
+import joblib
+import pandas as pd
+from FeaturesSetClass import CreateFeatures
+from sklearn.linear_model import LogisticRegression
+
+
+class TrainClassifier:
+
+	def __init__(self, lib_path):
+		self.lib_path = lib_path
+		self.Features = CreateFeatures()
+	
+	
+	def trainClassifier(self, data):
+		try:
+			classifier = joblib.load(os.path.join(self.lib_path,'niki-ai-tsk-model.pkl'))
+			#print(type(classifier))
+			#print("Training Model Object loaded..")
+		except:
+			result = []
+			for fl in range(0, len(data)):
+				print(fl)
+				feature = self.Features.CreateFeatureDataTrain(data['Sentence'][fl], data['Category'][fl])
+				result.append(feature)
+			data = pd.DataFrame(result)
+			train_y = data['Category']
+			train_x = data.drop(data[['Sentence', 'Category']], axis=1)
+			classifier = LogisticRegression(tol= 0.0001, dual =False,verbose=0, max_iter=100,multi_class='multinomial', warm_start=False, random_state=0,class_weight=None,C=0.1,solver='newton-cg', penalty='l2', fit_intercept=True, intercept_scaling=1, n_jobs=-1)
+			classifier.fit(train_x, train_y)
+			joblib.dump(classifier, os.path.join(self.lib_path,'niki-ai-tsk-model.pkl'))
+			#print("Training Model Object Written..")
+		return classifier
+	
+		
+if __name__ == '__main__':
+	path = 'C:\\Users\\Rushi\\Desktop\\INT-NOTES-DATA\\tsk\\LabelledData.csv' 			## path to the training data
+	input = 'C:\\Users\\Rushi\\Desktop\\INT-NOTES-DATA\\tsk\\input.csv'		#train_1000.label
+	result_path = 'C:\\Users\\Rushi\\Desktop\\INT-NOTES-DATA\\tsk\\Output.csv'			## path to output file
+	lib_path = 'C:\\Users\\Rushi\\Desktop\\INT-NOTES-DATA\\tsk\\model\\'				## path to the trained model
+	###
+	data = pd.read_csv(path, sep=",,,", header=None)
+	data=data.rename(columns = {0:'Sentence', 1:'Category'})
+	classObj = TrainClassifier(lib_path)
+	classifier = classObj.trainClassifier(data)
+	#print(type(classifier))
+	###  New Data Features Creation
+
+	test_data = pd.read_csv(input, sep="|", header=None, encoding='latin-1')
+	test_data=test_data.rename(columns = {0:'Sentence'})
+	test_x = []
+	for fl in range(0, len(test_data)):
+		#print(fl)
+		feature = classObj.Features.CreateFeatureDataTest(test_data['Sentence'][fl])
+		test_x.append(feature)
+	test_x = pd.DataFrame(test_x)
+	sent = test_x['Sentence']
+	test_x = test_x.drop(test_x[['Sentence']], axis=1)
+	probas = classifier.predict(test_x)
+	
+	catList = {0:'what', 1:'who', 2:'when', 3:'affirmation', 4:'unknown'}
+	probas = [catList[i] for i in probas]
+	result = pd.DataFrame({'Sentences':sent, 'Category':probas})
+	result.to_csv(result_path, sep='|', index=False, columns=['Sentences', 'Category'])
+	
+	
